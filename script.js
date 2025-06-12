a10ba4f508394e2fa35916a9502425f5b4dc2731
allu16kumoulica <134374716+allu16kumoulica@users.noreply.github.com>
2025-06-12 15:40:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-// Scroll animation
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        }
-    });
-});
 
-document.querySelectorAll('.fade-in').forEach((el) => observer.observe(el));
-
-// Dark mode toggle
-function toggleDarkMode() {
-    document.body.classList.toggle("dark-mode");
-}
